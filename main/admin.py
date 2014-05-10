500a8ce76b711187e74351343d22b5bd3b4a8c12
Jeff Bayes <jbayes92@gmail.com>
2014-05-10 16:05:42 -0700

    Potentially unncessary Restaurant class, definitely necessary MenuItem class

--- main/admin.py
+++ main/admin.py
@@ -1,1 +1,17 @@
 from django.contrib import admin
+from main.models import Restaurant, MenuItem
+
+class MenuInline(admin.StackedInline):
+    model = MenuItem
+    extra = 3
+
+class RestaurantAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {
+        'fields': ('restaurant_name', 'restaurant_cuisine', 'restaurant_price')
+        }),
+    ]
+    inlines = [MenuInline]
+    list_display = ('restaurant_name', 'restaurant_cuisine', 'restaurant_price')
+    
+admin.site.register(Restaurant, RestaurantAdmin)
--- main/views.py
+++ main/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import Restaurant, MenuItem
 
 class IndexView(TemplateView):
     ### Home page.
     template_name = "index.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['restaurants'] = Restaurant.objects.all()
+        return context
     
 """
 class ThrowawayView(TemplateView):
