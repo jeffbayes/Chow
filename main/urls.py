4f45388ec404063d94f3cadc00e7338dfc134488
Jeff Bayes <jbayes92@gmail.com>
2014-05-10 14:40:23 -0700

    Organized

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,6 @@ from django.views.generic import TemplateView
 from main import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.IndexView.as_view(), name='index')
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    ### url(r'^throwaway/$', views.ThrowawayView.as_view(), name='throwaway'),
 )
--- main/views.py
+++ main/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
-    """ Home page. """
+    ### Home page.
     template_name = "index.html"
+    
+"""
+class ThrowawayView(TemplateView):
+    ### Useful for testing things.
+    template_name = "throwaway.html"
+    """
 
